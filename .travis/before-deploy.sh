7cd250f55b28718cd90858138bf95ef63580714c
Rogelio J. Baucells <rogelio@STEELBRIDGE15.steelbridgelabs.local>
2017-06-07 15:37:45 -0400

    build scripts

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     # feedback
     echo "Processing signing key..."
     # decrypt signing key
-    openssl aes-256-cbc -K $encrypted_c88cfa69e519_key -iv $encrypted_c88cfa69e519_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_c93bc20805ed_key -iv $encrypted_c93bc20805ed_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     # import signing key
     gpg --fast-import .travis/codesigning.asc
 fi
